ab1bc21d07f7b8f25237cfb9585f30f14d8bf99c
Ksenia <ksju.nikitina@rambler.ru>
2021-09-03 20:50:07 +0300

    name + temp obj

--- source/get.js
+++ source/get.js
@@ -2,20 +2,22 @@
 
 /**
  * По полученному пути к вложенному свойству объекта вернуть значение этого свойства
- * @param {*} object - объект, значение свойства которого нужно вернуть
+ * @param {*} obj - объект, значение свойства которого нужно вернуть
  * @param {string} properties - путь к вложенному свойству
  * @returns {undefined|*} - возвращает значение свойства или undefined, если свойства нет
  */
 
-const get = (object, properties) => {
-    if (typeof object !== 'object' || typeof properties !== 'string')
+const get = (obj, properties) => {
+    if (!obj || typeof properties !== 'string')
         return;
 
+    let tempObj = {...obj};
+    
     const listOfProperties = properties.split('.');
     listOfProperties.forEach(item => {
-        if (object !== undefined && item)
-            object = object[item];
+        if (tempObj !== undefined && item)
+            tempObj = tempObj[item];
     })
 
-    return object;
+    return tempObj;
 }
